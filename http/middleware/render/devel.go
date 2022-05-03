7ec8d7842484e8a3102cacebae578000408969ed
Leon Mika <lmika@lmika.org>
2022-05-03 21:47:57 +1000

    Changed the listen to a template option

--- http/middleware/render/devel.go
+++ http/middleware/render/devel.go
@@ -7,42 +7,51 @@ import (
 	"strings"
 )
 
-func RebuildOnChange(ctx context.Context, cfg *Config, baseDir string) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
+// RebuildOnChange is a render option which will trigger the rebuild of the templates
+// when a .html file within baseDir or any subdirectories are changed.  If baseDir is
+// the empty string, this will do nothing.
+func RebuildOnChange(ctx context.Context, baseDir string) ConfigOption {
+	if baseDir == "" {
+		return func(config *Config) {}
 	}
 
-	go func() {
-		defer watcher.Close()
-
-		done := make(chan bool)
-		go func() {
-			for {
-				select {
-				case event, ok := <-watcher.Events:
-					if !ok {
-						return
-					}
-					if event.Op&fsnotify.Write == fsnotify.Write {
-						if strings.HasSuffix(event.Name, ".html") {
-							log.Printf("modified file: %v, rebuilding templates", event.Name)
-							cfg.rebuildTemplates()
-						}
-					}
-				case err, ok := <-watcher.Errors:
-					if !ok {
-						return
-					}
-					log.Println("error:", err)
-				}
-			}
-		}()
-
-		err = watcher.Add(baseDir)
+	return func(cfg *Config) {
+		watcher, err := fsnotify.NewWatcher()
 		if err != nil {
 			log.Fatal(err)
 		}
-		<-done
-	}()
+
+		go func() {
+			defer watcher.Close()
+
+			done := make(chan bool)
+			go func() {
+				for {
+					select {
+					case event, ok := <-watcher.Events:
+						if !ok {
+							return
+						}
+						if event.Op&fsnotify.Write == fsnotify.Write {
+							if strings.HasSuffix(event.Name, ".html") {
+								log.Printf("modified file: %v, rebuilding templates", event.Name)
+								cfg.rebuildTemplates()
+							}
+						}
+					case err, ok := <-watcher.Errors:
+						if !ok {
+							return
+						}
+						log.Println("error:", err)
+					}
+				}
+			}()
+
+			err = watcher.Add(baseDir)
+			if err != nil {
+				log.Fatal(err)
+			}
+			<-done
+		}()
+	}
 }
