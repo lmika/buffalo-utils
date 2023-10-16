4495e33a2481521ae818eb28b1a8f75e849d94fb
Leon Mika <lmika@lmika.org>
2023-10-16 23:39:30 +0000

    added MapWithError

--- fp/slices/map.go
+++ fp/slices/map.go
@@ -1,5 +1,6 @@
 package slices
 
+// Map returns a new slice containing the elements of ts transformed by the passed in function.
 func Map[T, U any](ts []T, fn func(t T) U) (us []U) {
 	if ts == nil {
 		return nil
@@ -10,4 +11,23 @@ func Map[T, U any](ts []T, fn func(t T) U) (us []U) {
 		us[i] = fn(t)
 	}
 	return us
+}
+
+// Map returns a new slice containing the elements of ts transformed by the passed in function, which
+// can either either a mapped value of U, or an error. If the mapping function returns an error, MapWithError
+// will return nil and the returned error.
+func MapWithError[T, U any](ts []T, fn func(t T) (U, error)) (us []U, err error) {
+	if ts == nil {
+		return nil, nil
+	}
+
+	us = make([]U, len(ts))
+	for i, t := range ts {
+		var e error
+		us[i], e = fn(t)
+		if e != nil {
+			return nil, e
+		}
+	}
+	return us, nil
 }
--- fp/slices/map_test.go
+++ fp/slices/map_test.go
@@ -1,6 +1,7 @@
 package slices_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/lmika/gopkgs/fp/slices"
@@ -20,5 +21,38 @@ func TestMap(t *testing.T) {
 		us := slices.Map(ts, func(x int) int { return x + 2 })
 
 		assert.Nil(t, us)
+	})
+}
+
+func TestMapWithError(t *testing.T) {
+	t.Run("should return a mapped slice with no error", func(t *testing.T) {
+		ts := []int{1, 2, 3}
+
+		us, err := slices.MapWithError(ts, func(x int) (int, error) { return x + 2, nil })
+
+		assert.Equal(t, []int{3, 4, 5}, us)
+		assert.NoError(t, err)
+	})
+
+	t.Run("should return nil with an error when mapping function returns an error", func(t *testing.T) {
+		ts := []int{1, 2, 3}
+
+		us, err := slices.MapWithError(ts, func(x int) (int, error) {
+			if x == 2 {
+				return 0, errors.New("bang")
+			}
+			return x + 2, nil
+		})
+
+		assert.Nil(t, us)
+		assert.Error(t, err)
+	})
+
+	t.Run("should return nil if passed in nil", func(t *testing.T) {
+		ts := []int(nil)
+		us, err := slices.MapWithError(ts, func(x int) (int, error) { return x + 2, nil })
+
+		assert.Nil(t, us)
+		assert.NoError(t, err)
 	})
 }
