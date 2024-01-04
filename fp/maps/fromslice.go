759f0d4c64219647749a9892c09c23db0aca405a
Leon Mika <lmika@lmika.org>
2024-01-04 04:27:15 +0000

    Added FromSlice

--- fp/maps/fromslice.go
+++ fp/maps/fromslice.go
@@ -1,5 +1,13 @@
 package maps
 
+func FromSlice[T any, K comparable, V any](ts []T, fn func(t T) (K, V)) map[K]V {
+	m, _ := FromSliceWithError(ts, func(t T) (k K, v V, _ error) {
+		k, v = fn(t)
+		return k, v, nil
+	})
+	return m
+}
+
 func FromSliceWithError[T any, K comparable, V any](ts []T, fn func(t T) (K, V, error)) (map[K]V, error) {
 	kvs := make(map[K]V)
 	for _, t := range ts {
