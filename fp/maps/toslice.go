2f09258c2466dbd19bbe204e3a32822520722e86
Leon Mika <lmika@lmika.org>
2024-04-08 04:13:57 +0000

    Added ToSliceWithError

--- fp/maps/toslice.go
+++ fp/maps/toslice.go
@@ -11,3 +11,19 @@ func ToSlice[K comparable, V, T any](m map[K]V, fn func(k K, v V) T) []T {
 	}
 	return ts
 }
+
+func ToSliceWithError[K comparable, V, T any](m map[K]V, fn func(k K, v V) (T, error)) ([]T, error) {
+	if m == nil {
+		return nil, nil
+	}
+
+	ts := make([]T, 0, len(m))
+	for k, v := range m {
+		w, err := fn(k, v)
+		if err != nil {
+			return nil, err
+		}
+		ts = append(ts, w)
+	}
+	return ts, nil
+}
